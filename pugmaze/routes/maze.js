18d1d4520c2a85c6191e87e44a52b9c600b22c80
Terry Howe <terrylhowe@gmail.com>
2017-06-01 12:08:41 -0600

    add params

--- pugmaze/routes/maze.js
+++ pugmaze/routes/maze.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('maze', { title: 'Express 3D Maze' });
+router.get('/:x/:y/:direction', function(req, res, next) {
+  res.render('maze', {
+    title: 'Express 3D Maze',
+    params: req.params,
+    textview: 'blah'
+  });
 });
 
 module.exports = router;
