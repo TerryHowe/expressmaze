4454d0b86a6f0bfb2776675cab32d4729407fa8b
Terry Howe <terrylhowe@gmail.com>
2017-06-22 05:21:32 -0600

    fix redirect

--- pugmaze/routes/index.js
+++ pugmaze/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/maze/0/0/1');
+  res.redirect('/maze/0/0/N');
 });
 
 module.exports = router;
